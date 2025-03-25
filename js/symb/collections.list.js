dee2cbde85eedc40505e74409550f5298f6230b1
Chandra Earl <sunray1@bellsouth.net>
2025-03-25 17:16:25 -0400

    safari address bar?

--- js/symb/collections.list.js
+++ js/symb/collections.list.js
@@ -49,7 +49,9 @@ function toggleFieldBox(target){
 function openIndPU(occId,clid){
 	var wWidth = 1100;
 	if(document.body.offsetWidth < wWidth) wWidth = document.body.offsetWidth*0.9;
-	var newWindow = window.open('individual/index.php?occid='+occId+'&clid='+clid,'indspec' + occId,'scrollbars=1,toolbar=0,resizable=1,width='+(wWidth)+',height=700,left=20,top=20');
+	// neon edit
+	var newWindow = window.open('individual/index.php?occid='+occId+'&clid='+clid,'indspec' + occId,'scrollbars=1,toolbar=0,resizable=1,location=1,width='+(wWidth)+',height=700,left=20,top=20');
+	// end edit
 	if (newWindow.opener == null) newWindow.opener = self;
 	return false;
 }
