39a54dcdfa55c4df5da4b84f00ff337314d39aae
Chandra Earl <sunray1@bellsouth.net>
2025-05-21 16:31:27 -0400

    testing loan

--- classes/OccurrenceManager.php
+++ classes/OccurrenceManager.php
@@ -486,6 +486,10 @@ class OccurrenceManager extends OccurrenceTaxaManager {
 			$sqlWhere .= 'AND (o.occid IN(SELECT occid FROM omoccurgenetic)) ';
 			$this->displaySearchArr[] = $this->LANG['HAS_GENETIC_DATA'];
 		}
+		if(array_key_exists("availableforloan",$this->searchTermArr)){
+			$sqlWhere .= "AND (o.availability = 1) ";
+			$this->displaySearchArr[] = 'available for loan';
+		}
 		if(array_key_exists('hascoords', $this->searchTermArr)){
 			$sqlWhere .= 'AND (o.decimalLatitude IS NOT NULL) ';
 			$this->displaySearchArr[] = $this->LANG['HAS_COORDINATES'];
