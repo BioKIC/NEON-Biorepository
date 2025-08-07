3c8554d5b949e57c1c9b08b4ae39e777c7093bb1
Kelsey Yule <kelsey.yule@gmail.com>
2025-08-06 20:31:58 -0700

    missing </div>

--- collections/individual/index.php
+++ collections/individual/index.php
@@ -546,6 +546,7 @@ $traitArr = $indManager->getTraitArr();
 									if ($IS_ADMIN && ($catTag == 'Barcode (sampleCode)' || $catTag == 'Sample Tag (sampleID)')) {
 										echo ' <span style="margin-left: 10px"><a href="../../neon/shipment/manifestviewer.php?quicksearch=' . $occid . '" target="_blank">Go to Manifest</a></span>';
 									}
+									echo '</div>';
 										//End NEON customization
 								}
 								?>
@@ -1154,7 +1155,7 @@ $traitArr = $indManager->getTraitArr();
 									?>
 									<div>
 										<label><?= $LANG['SOURCE_URL'] ?>:</label>
-										<a href="<?= $occArr['source']['url'] ?>" target="_blank"></label> <?=$occArr['source']['url'] ?></a>
+										<a href="<?= $occArr['source']['url'] ?>" target="_blank"><?= $occArr['source']['url'] ?></a>
 									</div>
 									<div><label><?= $LANG['SOURCE_MANAGEMENT'] ?>:</label> <?= $sourceManagement ?></div>
 									<?php
