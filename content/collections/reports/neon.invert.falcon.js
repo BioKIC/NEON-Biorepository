994890ee2cddac02e1aa0f6119c53dda6b0b2014
Chandra Earl <sunray1@bellsouth.net>
2025-08-21 14:17:57 -0400

    change barcode to be sampleCode

--- content/collections/reports/neon.invert.falcon.js
+++ content/collections/reports/neon.invert.falcon.js
@@ -9,13 +9,24 @@
 
 let labels = document.querySelectorAll(".label");
 labels.forEach((label) => {
+
   let otherCatNums = label.querySelector(".other-catalog-numbers");
   if (otherCatNums) {
     otherCatNums.remove();
   }
-  
+
   let other = label.querySelector(".othercatalognumbers");
   if (other) {
+    let codeMatch = other.innerText.match(/NEON sampleCode \(barcode\):\s*([^;]+)/);
+    let bcImg = label.querySelector(".cn-barcode img");
+
+    if (codeMatch && bcImg) {
+      let sampleCode = codeMatch[1].trim();
+      bcImg.src = bcImg.src.replace(/(bctext=)[^&]+/, "$1" + encodeURIComponent(sampleCode));
+    } else if (bcImg) {
+      bcImg.remove();
+    }
+
     let match = other.innerText.match(/NEON sampleID:\s*([^;]+)/);
     if (match) {
       other.innerText = match[1].trim();
