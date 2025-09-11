a6c50bd34af9f5dd9cf94c6a36c91f7fe7222d4f
Chandra Earl <sunray1@bellsouth.net>
2025-09-11 14:44:09 -0400

    Update browsechecklists.php

--- projects/browsechecklists.php
+++ projects/browsechecklists.php
@@ -86,7 +86,7 @@ header('Content-Type: text/html; charset=' . $CHARSET);
 			<div id = "heroimage-div">
 				<img src="<?php echo $CLIENT_ROOT . '/images/card-images/Beetles_pinned.jpg'; ?>" style="max-width:100%" alt="Pinned Beetles" loading="lazy">
 			</div>
-			<p>Species checklists are intended to help users identify taxa at and around NEON field sites. Each checklist provides a <b>comprehensive species list, visual resources, and specimens</b> collected within a site or domain.</p>
+			<p>Species checklists are intended to help users identify taxa at and around NEON field sites. Each checklist provides a <b>comprehensive species list, images, and specimens</b> collected within a site or domain.</p>
 			<div id="biorepo-checklists-content"></div>
 		</div>
 	</body>
