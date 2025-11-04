411a93969cfabeb7903600db0a00a72c61f8f82e
Chandra Earl <sunray1@bellsouth.net>
2025-11-04 14:16:15 -0500

    revert silent authentication code

--- profile/logout.php
+++ profile/logout.php
@@ -9,25 +9,6 @@ $localSessionID = $profManager->lookupLocalSessionIDWithThirdPartySid($sid);
 //neon edit
 $localSessionID = $_COOKIE['PHPSESSID'] ?? '';
 $redirectUrl = GeneralUtil::getDomain() . $CLIENT_ROOT . '/index.php';
-
-$_SESSION = [];            // empty the session array
-
-// remove the session cookie from the browser
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(),
-        '',
-        time() - 42000,
-        $params["path"],
-        $params["domain"],
-        $params["secure"],
-        $params["httponly"]
-    );
-}
-
-// destroy the session file on the server
-session_destroy();
 //end edit
 
 if($localSessionID){
