c09bf3ff3d294e37b431f2764f3b33f442204535
Edward Gilbert <egbot@asu.edu>
2026-02-20 16:31:43 -0500

    Increase memory limit on downloads

--- classes/DwcArchiverCore.php
+++ classes/DwcArchiverCore.php
@@ -90,7 +90,7 @@ class DwcArchiverCore extends Manager{
 			'georeferenceVerificationStatus', 'habitat'
 		);
 
-		//ini_set('memory_limit','512M');
+		ini_set('memory_limit','512M');
 		set_time_limit(1800);
 	}
 
