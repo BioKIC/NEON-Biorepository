b146ff3089f679d9171ff906d852136407edf39a
Chandra Earl <sunray1@bellsouth.net>
2026-06-06 17:15:03 -0400

    Moved Requesting Samples section to samplerequests.php

--- misc/sampleguidelines.php
+++ misc/sampleguidelines.php
@@ -39,7 +39,6 @@ header("Content-Type: text/html; charset=" . $CHARSET);
 	</article>
 		
 	<ol>
-		<li><a href="#requesting-samples">Requesting Samples</a></li>
 		<li><a href="#proposals-and-letters-of-collaboration-or-support">Proposals and Letters</a></li>
 		<li><a href="#visiting-the-neon-biorepository">Visiting the NEON Biorepository</a></li>
 		<li><a href="#sample-use-agreement">Sample Use Agreement</a></li>
@@ -48,20 +47,6 @@ header("Content-Type: text/html; charset=" . $CHARSET);
 		<li><a href="#citations">NEON Biorepository Citation Requirements</a></li>
 	</ol>
 	<hr>
-
-		<article>
-			<h2 class="anchor" id="requesting-samples">Requesting Samples</h2>
-			
-			<img src="images/loan_process.png" alt="Sample loan process" style="max-width: 100%; height: auto; margin: 1rem 0;">
-
-			<p>To request samples or learn more about what samples are available for use, you may either browse the <a href="https://biorepo.neonscience.org/portal/collections/misc/browsecollprofiles.php" target="_blank"">Sample Type Browser</a> or <a href="https://biorepo.neonscience.org/portal/neon/search/index.php" target="_blank">NEON Biorepository Sample Portal</a> and initiate inquiries by <a href="https://www.neonscience.org/about/contact-neon-biorepository" target="_blank">contacting us</a> directly. We will follow up within five business days to assist you in identifying appropriate samples and submitting a Sample Use Request.</p>
-
-			<p>All requests are evaluated via the <a href="#sample-use-approval-process">Sample Use Approval Process</a>, subject to the <a href="#sample-use-policy">Sample Use Policy</a>. <b><i>All requests require a mutually developed and signed Sample Use Agreement prior to sample processing and shipment or access.</i></b> Researchers should allow approximately two weeks for development and finalization of the Sample Use Agreement.</p>
-
-			<p>The NEON Biorepository aims to fulfill requests of fewer than 100 samples that require no subsampling or additional processing within four weeks of signing a Sample Use Agreement and without any charge. Note that processing times can be much longer when hiring additional NEON Biorepository personnel is required.</p>
-
-			<p>Funding is not required to request or access samples. However, requests requiring substantial effort to fulfill may require funding or service fees. These costs will be estimated by the NEON Biorepository based on the type of request, complexity, and amount of support required. Researchers are encouraged to visit the NEON Biorepository to access samples, and we will provide working space, sample access, and our expertise free of charge.</p>
-		</article>
 
 		<article>
 			<h2 class="anchor" id="proposals-and-letters-of-collaboration-or-support">Proposals and Letters of Collaboration or Support</h2>
