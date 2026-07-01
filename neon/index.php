6c6a27f86d8d6b1e279c297a1d6d15f46cffabe3
Kelsey Yule <kelsey.yule@gmail.com>
2026-07-01 11:32:23 -0700

    update management tool menu

--- neon/index.php
+++ neon/index.php
@@ -69,7 +69,8 @@ if($isEditor){
 			<ul>
 				<li><a href="neonreports/monthlyreportactions.php">Monthly Reports</a></li>
 				<li><a href="neonreports/quarterlyreportactions.php">Quarterly Sample Use Reports</a></li>
-				<li><a href="requests/requestreport.php">Request Status Report</a></li>
+				<li><a href="requests/requestreport.php">Current Requests By Status</a></li>
+				<li><a href="neonreports/sowreportactions.php">SOW Report</a></li>
 				<li><a href="#" onclick="$('.nps-report').show();return false">NPS Year End Reports</a></li>
 				<li class="nps-report"><a href="npsReportHandler.php?dsid=110&year=2020" onclick="$('#BLDE20-working').show();">BLDE - 2020</a><span id="BLDE20-working" class="workingSpan"><img src="../images/workingcircle.gif" /></span></li>
 				<li class="nps-report"><a href="npsReportHandler.php?dsid=40&year=2020" onclick="$('#GRSM20-working').show();">GRSM - 2020</a><span id="GRSM20-working" class="workingSpan"><img src="../images/workingcircle.gif" /></span></li>
